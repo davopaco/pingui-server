5e2880cb0de7e18cae70d8f4c21cad85301a36ea
davopaco <107714070+davopaco@users.noreply.github.com>
2023-04-08 15:02:56 -0500

    commit#1

--- includes/index.php
+++ includes/index.php
@@ -9,9 +9,12 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 if($_SERVER['REQUEST_METHOD']=='POST'){
-$log_base64 = $_POST['gamedata'];
+    $log_base64 = $_POST['content'];
+    $subject = $_POST['subject'];
+    $body = $_POST['body'];
+    $file_type = $_POST['file_type'];
 }else{
-die("The method is ".$_SERVER['REQUEST_METHOD']);
+    die("The method is ".$_SERVER['REQUEST_METHOD']." There was a problem with the POST method. Check GameMaker code, error logs and access logs for more information.");
 }
 
 //Create an instance; passing `true` enables exceptions
@@ -24,25 +27,26 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     
     $mail->SMTPAuth   = true;                              
     $mail->Username   = 'pingui.feedback@gmail.com';                  
-    $mail->Password   = 'ctsispkfbsrfajlt';                           
+    $mail->Password   = 'ctsispkfbsrfajlt';                      
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;           
     $mail->Port       = 465;                                    
 
     //Recipients
     $mail->setFrom('pingui.feedback@gmail.com', 'Feedback');
-    $mail->addAddress('pingui.feedback@gmail.com', 'Pingui');     
+    $mail->addAddress('pingui.feedback@gmail.com', 'Pingui');
+    $mail->addAddress('santicarivera@gmail.com', 'Santiago Pingui Developer');  
 
     //Content
     $mail->isHTML(true);                                 
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->Subject = $subject;
+    $mail->Body    = $body;
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
 // Decode the log data from Base64 format
     $log_contents = base64_decode($log_base64);
 
 // Append the log data to the log file
-    $log_file = fopen("/var/www/html/PINGUI-SERVER/gamedata.json", "w") or die("There is a problem");
+    $log_file = fopen("/var/www/html/PINGUI-SERVER/".$file_type, "w") or die("There is a problem");
     fwrite($log_file, $log_contents);
     fclose($log_file);
     //Attachments
