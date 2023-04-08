1b76d67287e0becc2bf788ff41966113dcef7b53
root <root@pingui-server.us-east1-b.c.pingui-php-server.internal>
2023-04-08 18:52:09 +0000

    Changed the index file for matching with gamemaker

--- includes/index.php
+++ includes/index.php
@@ -1,19 +1,21 @@
 <?php
 #Include PHP files PHPMAILER
-require __DIR__.'PHPMailer.php';
-require __DIR__.'Exception.php';
-require __DIR__.'SMTP.php';
+require __DIR__.'/PHPMailer.php';
+require __DIR__.'/Exception.php';
+require __DIR__.'/SMTP.php';
 
 #Define name spaces
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
+if($_SERVER['REQUEST_METHOD']=='POST'){
+$log_base64 = $_POST['gamedata'];
+}else{
+die("The method is ".$_SERVER['REQUEST_METHOD']);
+}
 
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
-if(isset($_GET["path"])){
-    $path = $_GET["path"];
-}
 
 try {
     //Server settings
@@ -36,8 +38,15 @@ try {
     $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
+// Decode the log data from Base64 format
+    $log_contents = base64_decode($log_base64);
+
+// Append the log data to the log file
+    $log_file = fopen("/var/www/html/PINGUI-SERVER/gamedata.json", "w") or die("There is a problem");
+    fwrite($log_file, $log_contents);
+    fclose($log_file);
     //Attachments
-    //$mail->addAttachment($path, 'constraint.txt'); 
+    $mail->addAttachment("/var/www/html/PINGUI-SERVER/gamedata.json", 'feed.json'); 
 
     $mail->send();
     echo 'Message has been sent';
